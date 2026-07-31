122b7a23277d257565c3af4e0ff82ca9885c5189
Aravindan RR <rraravindan22@gmail.com>
2026-07-31 18:40:16 +0530

    Lab Test_Day 5

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,27 +27,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+     baseURL: 'https://example.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     video: 'on',
-    // video: {
-    //   mode: 'on-first-retry',
-    //   size: { width: 640, height: 480 },
-    //   show: {
-    //     actions: {
-    //       duration: 500,
-    //       position: 'top-right',
-    //       fontSize: 14,
-    //     },
-    //     test: {
-    //       level: 'step',
-    //       position: 'top-left',
-    //       fontSize: 12,
-    //     }
-    //   },
-    // },
+    
   },
 
   /* Configure projects for major browsers */
@@ -57,15 +42,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- src/tests/LAB/Fifth_test_lab.spec.ts
+++ src/tests/LAB/Fifth_test_lab.spec.ts
@@ -1,8 +1,48 @@
 import { test , expect } from '@playwright/test';
 
-   test('Action and Assertions', async ({ page }) => {
-    await page.goto('https://testautomationpractice.blogspot.com/');
-    await expect(page).toHaveTitle('Automation Testing Practice');
-   
+ test('Demo test_1', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
 
+test.fail('Demo test_2', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test.skip('Demo test_3', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test.fixme('Demo test_4', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test('Demo test_5', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Example Domain/);
+});
+
+test.describe('two tests', () => {
+  test('one', async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveTitle(/Example Domain/);
+  });
+
+  test('two', async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveTitle(/Example Domain/);
+  });
+});
+
+test('example test', async ({ page, browser }) => {
+  test.info().annotations.push({
+    type: 'browser version',
+    description: browser.version(),
+  });
+
+   await page.goto('/');
+   await expect(page).toHaveTitle(/Example Domain/);
 });
